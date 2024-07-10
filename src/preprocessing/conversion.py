aa3fe3d8d6a0fca8bed0fb06a9509bb67711a79c
Rohith Krishnan <rbala@hs-nordhausen.de>
2024-07-10 14:03:29 +0200

    Update conversion.py

--- src/preprocessing/conversion.py
+++ src/preprocessing/conversion.py
@@ -14,9 +14,11 @@ CAUTION:
 !!!!!!!!!!!!!!!!!!!!!!!!!!!
 this script consists:
     - function to convert timeseries from any resolution to hourly values, does'nt matter which type of data is given to the funtion (can be a series/DF/list/etc.)' 
+    - function to calculate the investment, operating and equivalent periodic costs 
         
 """
 import pandas as pd
+from oemof.tools import economics
 
 def convert_into_hourly_values (data, simulation_year, existing_res = '15min'):
     
@@ -40,3 +42,31 @@ def convert_into_hourly_values (data, simulation_year, existing_res = '15min'):
             data= data.set_index(d_t_index)
             data = data.resample('H').mean()
             return(data)
+        
+
+def investment_parameter(data,simulation_year):
+
+    '''
+    data : dictionary with all the techno-economical parameters (csv file)
+    simulationyear: to choose the specific parameter for the respective simulation year
+    
+    Function to calculate epc costs and store it as a variable for all technologies
+    
+    Note: Fix a standard file name format for technology parameters.
+    '''   
+    T_list = []             # Technology list will be automatically generated based on the file name
+    for i in data:
+        if i.startswith('Parameter'):
+            i = i[10:]
+            i = i[:-5]
+            T_list.append(i) 
+
+    my_dict={}
+    for name in T_list:
+        n = "Parameter_"+ name +'_' + str(simulation_year)
+        my_dict[name]= {}
+        my_dict[name]['investk'] = economics.annuity(capex=data[n][name]['CAPEX'], n=data[n][name]['Amortisierungszeit'], wacc=data['Systemeigenschaften']['System']['Zinssatz']/100)
+        my_dict[name]['betriebsk'] = data[n][name]['CAPEX'] * (data[n][name]['OPEX']/100)
+        my_dict[name]['epc'] = my_dict[name]['investk'] + my_dict[name]['betriebsk']
+        
+    return(my_dict)
