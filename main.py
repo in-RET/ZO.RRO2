06038781e11b7c79f9dde1156d934459d1eb4f80
Rohith Krishnan <rbala@hs-nordhausen.de>
2024-12-12 12:21:20 +0100

    2030_BS0001_005

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
 
     sim_data, result,csv = solveModels(
         variations=["BS0001"],
-        scenario_num = "004",
+        scenario_num = "005",
         years=[2030],# 2040, 2050],
         model_name="Basic_example_zorro_1",
         solver="gurobi",
@@ -18,10 +18,10 @@ if __name__ == "__main__":
         solver_output=False,
         print_graph=False,
         Anteilig_erneuerbar = True,
-        hypothese = "Influence of new PV feed-in profiles developed using PVlib",
-        sim_remarks = "- The PV feed-in profiles were developed with the help of PVlib package (open-source). The weather data is acquired from Meteonorm at 10 m height "+
-                        "and previouly was imported in PVsyst to develop the energy yield timeseries. PVlib uses a different algorithm but the same module and inverter specifications were implemented in PVlib to get similar energy yield.\n"+
-                        "- Other parameters remain the same [[2030_BS0001_003]]"
+        hypothese = "Influence of new load profiles developed using open-source tools like RAMP, RAMP mobility, RC building simulator....",
+        sim_remarks = "- The load profiles were developed with the help of several open-source packages to make it more dynamic in comparison to the Standart loadprofiles. The weather data is acquired from Meteonorm at 10 m height "+
+                        "for the planing regions imported in the tools to deelop synthetic load profiles. Most tools work with stochastic algorithm to develop the time series. \n"+
+                        "- Other parameters remain the same [[2030_BS0001_004]]"
     )
 
     # %TODO: Auswertung der Dump-Daten (csv-daten erstellen, automatische grafiken etc.)
