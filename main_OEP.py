531503babb029bc8ae71515611bc542701f8b0f7
Rohith <117290400+rkrish1405@users.noreply.github.com>
2024-12-20 21:52:56 +0100

    Last changes before christmas
    from AO

---
+++ main_OEP.py
@@ -0,0 +1,112 @@
+import os
+import json
+import pandas as pd
+
+from random import randint
+from src.oep_handler import OepHandler, create_tabledata, create_tableschema, create_metadata
+
+WITH_UPLOAD = False
+
+topic =  "sandbox" #"model_draft"
+token = os.environ.get("OEP_API_TOKEN") #% TODO: .env-File anlegen nicht vergessen!
+print("Gewählter Token:", token)
+
+data_path = os.path.abspath(os.path.join("data"))
+print("Datenpfad:", data_path)
+
+general_meta = pd.read_csv(
+    filepath_or_buffer=os.path.join(data_path, "meta_data_general.csv"),
+    index_col=0,
+    sep=";",
+    decimal=".",
+)
+
+for root, dirs, files in os.walk(data_path, topdown=True):
+    pass # TODO for later
+
+raw_file_name = "parameter_onshore_wind_power_plant_inret.csv"
+table = f"%s_{randint(0, 100000)}" % raw_file_name.replace(".csv", "")
+raw_csv = pd.read_csv(
+    filepath_or_buffer=os.path.join(data_path, "scalars", "parameter", raw_file_name),
+    index_col=0,
+    sep=';',
+    decimal='.',
+    encoding = 'unicode_escape'
+)
+
+# Create API-Handler-Object
+OepApi = OepHandler(f"https://openenergyplatform.org/api/v0/schema/{topic}/tables/{table}/", token)
+
+raw_data = raw_csv.loc["data", :] # Pandas ist geil
+raw_meta = raw_csv.loc[["data_type", "type", "unit", "description", "primary_key"], :]
+
+########################################################################################################################
+#   Create Table Schema
+########################################################################################################################
+table_schema = create_tableschema(raw_data, raw_meta)
+
+########################################################################################################################
+#   Upload Table Schema
+########################################################################################################################
+if WITH_UPLOAD:
+    response = OepApi.create_table(table_schema)
+    print(response)
+
+########################################################################################################################
+#   Create Table Data
+########################################################################################################################
+table_data = create_tabledata(raw_data)
+
+########################################################################################################################
+#   Upload Table Data
+########################################################################################################################
+if WITH_UPLOAD:
+    response = OepApi.upload_data(table_data)
+    print(response)
+
+########################################################################################################################
+#   Create Meta Data
+########################################################################################################################
+meta_fields = create_metadata(raw_meta)
+
+meta_data = general_meta.to_dict()["value"]
+meta_data["id"] = table
+meta_data["name"] = table.replace("_", " ")
+meta_data["keywords"] = []
+meta_data["subject"] = ["Debugging purposes"],
+meta_data["languages"] = ["EN", "DE"]
+meta_data["licenses"] = [
+    {
+        "name": "CC-BY-4.0",
+        "path": "https://spdx.github.io/license-list-data/CC-BY-4.0.html",
+        "title": "Creative Commons Attribution 4.0 International"
+    }
+]
+meta_data["context"] = OepApi.context
+meta_data["resources"] = [{
+    "name": table,
+    "schema": {
+        "fields": meta_fields
+    }
+}]
+
+with open('meta_debug.json', 'w', encoding='utf-8') as f:
+    json.dump(meta_data, f, ensure_ascii=False, indent=2)
+
+########################################################################################################################
+#   Upload Meta Data
+########################################################################################################################
+if WITH_UPLOAD:
+    response = OepApi.upload_metadata(meta_data)
+    print(response)
+
+print(f"https://openenergyplatform.org/dataedit/view/{topic}/{table}")
+
+
+
+
+
+
+
+
+
---
+++ src/oep_handler.py
@@ -0,0 +1,95 @@
+import requests as req
+import numpy as np
+import json
+
+from jsonlint import meta
+
+
+def create_tableschema(data, meta) -> dict[str, list[dict]]:
+    table_schema_data = []
+
+    for key in data.keys():
+        table_schema_data.append({
+            "name": key,
+            "data_type": meta.loc["data_type", key],
+            "primary_key": meta.loc["primary_key", key] if meta.loc["primary_key", key] is not np.nan else None
+        })
+
+    return {"columns": table_schema_data}
+
+def create_tabledata(data) -> list[dict]:
+    return json.loads(data.to_json(orient="records"))
+
+def create_metadata(data) -> (list[dict], list, list):
+    tmp_json = json.loads(data.to_json(orient="columns"))
+
+    meta_data = []
+
+    for index in tmp_json:
+        data_dict = tmp_json[index]
+        data_dict["name"] = index
+
+        if not bool(data_dict["description"]):
+            data_dict["description"] = ""
+
+        if not bool(data_dict["unit"]):
+            data_dict["unit"] = ""
+
+        if bool(data_dict["primary_key"]):
+            data_dict["primary_key"] = bool(tmp_json[index]["primary_key"])
+        else:
+            del(data_dict["primary_key"])
+
+        meta_data.append(data_dict)
+
+    return meta_data
+
+def translate_response(func: str, response: req.Response):
+    if not response.ok:
+        return f'%s: %s' % (func, response.text)
+    else:
+        return f'%s: Request successful.' % func
+
+
+class OepHandler:
+    auth_header = None
+    api_url = None
+    context = {
+        "contact": "Hochschule Nordhausen - Institut für regenerative Energietechnik",
+        "homepage": "https://hs-nordhausen.de",
+        "documentation": "https://hs-nordhausen.de",
+        "sourceCode": "https://github.com/in-RET"
+    }
+
+    def __init__(self, api_url, token):
+        self.api_url = api_url
+        self.auth_header = {"Authorization": "Token %s" % token}
+
+        print("AUTH_HEADER", self.auth_header)
+        print("API_URL:",  self.api_url)
+
+
+    def create_table(self, table_schema: dict[str, list[dict]]) -> str:
+        return translate_response(
+            func="create_table",
+            response=req.put(self.api_url, json={"query": table_schema}, headers=self.auth_header)
+        )
+
+    def delete_table(self) -> str:
+        return translate_response(
+            func="delete_table",
+            response=req.delete(self.api_url, headers=self.auth_header)
+        )
+
+    def upload_data(self, data: list[dict[str, str]]) -> str:
+        return translate_response(
+            func="upload_data",
+            response=req.post(self.api_url + "rows/new", json={"query": data}, headers=self.auth_header)
+        )
+
+    def upload_metadata(self, data) -> str:
+        return translate_response(
+            func="upload_metadata",
+            response=req.post(self.api_url + "meta/", json=data, headers=self.auth_header)
+        )
+
